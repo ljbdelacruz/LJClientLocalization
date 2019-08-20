87e10163ca0b8a3a5ea88e3d0f3a4ca46b53914f
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2019-08-21 07:41:18 +0800

    Initial Commit

---
+++ LJClientLocalization/LJClientLocalization.h
@@ -0,0 +1,19 @@
+//
+//  LJClientLocalization.h
+//  LJClientLocalization
+//
+//  Created by Lainel John Dela Cruz on 21/08/2019.
+//  Copyright © 2019 Lainel John Dela Cruz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LJClientLocalization.
+FOUNDATION_EXPORT double LJClientLocalizationVersionNumber;
+
+//! Project version string for LJClientLocalization.
+FOUNDATION_EXPORT const unsigned char LJClientLocalizationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LJClientLocalization/PublicHeader.h>
+
+
